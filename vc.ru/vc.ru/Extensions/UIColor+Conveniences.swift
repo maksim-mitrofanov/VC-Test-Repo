ea47c4321a2b2538b1f3a0e7b6219c4271229023
maksim_mitrofanov <keyseymiroff@gmail.com>
2024-02-18 19:48:14 +0400

    General prep work.
    1. App icon fix. Original image used (from the website). Cropped and upscaled while maintaining aspect ratio.
    2. Launch screen fix. Launch screen background color matches vc.ru brand color + brand icon added to the center of the screen. Layout constraints added.
    3. App only runs in vertical orientation. Can’t be used on mac / iPad.
    4. Remove «Main.storyboard» file. Added «UIColor + convenience» file.  Added «background color» static property on UIColor. Modified SceneDelegate and AppDelegate to support app launch without «Main.storyboard» file.
    
    Total time - 22 min.

---
+++ vc.ru/vc.ru/Extensions/UIColor+Conveniences.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Conveniences.swift
+//  vc.ru
+//
+//  Created by Максим Митрофанов on 18.02.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "Backgroud Color")
+}
